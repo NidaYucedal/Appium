157ca0bee2c6c265991d4799f1f74de8d5f90ec6
Nida Yucedal <yucedalnida@gmail.com>
2022-12-22 12:20:36 +0300

    first

--- src/test/java/mobiltesting/Appium06Amazon.java
+++ src/test/java/mobiltesting/Appium06Amazon.java
@@ -29,7 +29,6 @@ public class Appium06Amazon {
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
 
-
     }
 
 }
