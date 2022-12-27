e1847bcd49c171389d9a05a80bc6fe31839b9870
Nida Yucedal <yucedalnida@gmail.com>
2022-12-27 19:27:19 +0300

    new

---
+++ src/test/java/ECommerceApp/BaseECommerceApp.java
@@ -0,0 +1,28 @@
+package ECommerceApp;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseECommerceApp {
+    @Test
+    public static AndroidDriver getAndroidDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        //capabilities.setCapability(MobileCapabilityType.AUTO_WEBVIEW,"true");
+        capabilities.setCapability("chromedriverExecutable","C:\\Users\\Administrator\\IdeaProjects\\AppiumPractise\\src\\driver\\chromedriver.exe");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Administrator\\IdeaProjects\\AppiumPractise\\src\\apps\\General-Store.apk");
+        capabilities.setCapability("noReset", "true");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
+    }
+}
