b256b4a86475a5f59509dcd787c133c7f15ff48e
Nida Yucedal <yucedalnida@gmail.com>
2022-12-28 18:33:16 +0300

    new

---
+++ src/test/java/parallel/parallelAndroidTest02.java
@@ -0,0 +1,45 @@
+package parallel;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class parallelAndroidTest02 extends parallelAndroidTest {
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+
+        AndroidDriver<MobileElement> driver = androidDriver("emulator-5554", "10.0", "emulator-5554", "10001", "4724");
+
+
+        driver.findElementByXPath("//android.widget.TextView[@text='API Demos']").click();
+        Thread.sleep(3000);
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        Thread.sleep(5000);
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Switches\"))");
+        driver.findElementByXPath("//android.widget.TextView[@text='Switches']").click();
+
+        driver.quit();
+
+    }
+
+
+    @Test
+    public void test2() throws MalformedURLException, InterruptedException {
+        AndroidDriver<MobileElement> driver = androidDriver("RealDevice", "10.0", "1312d4f50606", "10001", "4723");
+
+
+        driver.findElementByXPath("//android.widget.TextView[@text='API Demos']").click();
+        Thread.sleep(3000);
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        Thread.sleep(5000);
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Switches\"))");
+        driver.findElementByXPath("//android.widget.TextView[@text='Switches']").click();
+        Thread.sleep(3000);
+        driver.quit();
+
+
+    }
+}
