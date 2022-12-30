debc821dd1f86917342ce4d6ee3dc8564cfcb88c
Nida Yucedal <yucedalnida@gmail.com>
2022-12-30 20:50:04 +0300

    new

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,21 +1,25 @@
 package utilities;
 
 
-import io.appium.java_client.MobileBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static utilities.Driver.getAppiumDriver;
 
 
 public class ReusableMethods {
@@ -33,7 +37,7 @@ public class ReusableMethods {
 
     //elementlerin textlerine gore tiklar
     public static void tapOnElementWithText(String text) {
-        List<MobileElement> mobileElementList = Driver.getAppiumDriver().findElementsByClassName("android.widget.TextView");
+        List<MobileElement> mobileElementList = getAppiumDriver().findElementsByClassName("android.widget.TextView");
         for (MobileElement page : mobileElementList) {
             if (page.getText().equals(text)) {
                 page.click();
@@ -47,7 +51,7 @@ public class ReusableMethods {
     //ikinci alternatif bir method
     public static void clickOnElementWithText(String elementText) throws InterruptedException {
         Thread.sleep(4000);
-        List<MobileElement> mobileElementList = Driver.getAppiumDriver().findElementsByXPath("//android.widget.TextView[@text='" + elementText + "']");
+        List<MobileElement> mobileElementList = getAppiumDriver().findElementsByXPath("//android.widget.TextView[@text='" + elementText + "']");
         if (mobileElementList.size() > 0) {
             mobileElementList.get(0).click();
         } else scrollWithUiScrollable(elementText);
@@ -59,7 +63,7 @@ public class ReusableMethods {
     //visible olmasini bekle true don
     public static boolean isElementPresent(String text) {
         boolean elementFound = false;
-        List<MobileElement> mobileElementList = Driver.getAppiumDriver().findElementsByXPath("//android.widget.TextView[@text='" + text + "']");
+        List<MobileElement> mobileElementList = getAppiumDriver().findElementsByXPath("//android.widget.TextView[@text='" + text + "']");
         for (MobileElement el : mobileElementList) {
             if (el.getText().equals(text)) {
                 waitToBeVisible(el, 10);
@@ -113,24 +117,24 @@ public class ReusableMethods {
 
     //element gorunene kadar bekle
     public static void waitToBeVisible(MobileElement element, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.getAppiumDriver(), timeout);
+        WebDriverWait wait = new WebDriverWait(getAppiumDriver(), timeout);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static void waitToBeClickable(MobileElement element, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.getAppiumDriver(), timeout);
+        WebDriverWait wait = new WebDriverWait(getAppiumDriver(), timeout);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static void scrollWithUiScrollable(String elementText) {
-        AndroidDriver<MobileElement> driver = (AndroidDriver) Driver.getAppiumDriver();
+        AndroidDriver<MobileElement> driver = (AndroidDriver) getAppiumDriver();
         driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"))");
         tapOn(driver.findElementByXPath("//android.widget.TextView[@text='" + elementText + "']"));
     }
 
 
     public static void setNumber(String no) {
-        TouchAction touchAction = new TouchAction<>(Driver.getAppiumDriver());
+        TouchAction touchAction = new TouchAction<>(getAppiumDriver());
         String[] sayi = no.split("");
         System.out.println(Arrays.toString(sayi));
         for (int i = 0; i < sayi.length; i++) {
@@ -174,72 +178,102 @@ public class ReusableMethods {
 
     public static void waitToast(MobileElement element, int timeout) {
         // Set the implicit wait timeout to 10 seconds
-        WebDriverWait wait = new WebDriverWait(Driver.getAppiumDriver(), timeout);
+        WebDriverWait wait = new WebDriverWait(getAppiumDriver(), timeout);
         wait.until(ExpectedConditions.visibilityOf(element));
 
         // Wait until the toast message becomes visible
-        Driver.getAppiumDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getAppiumDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Toast[1]")));
-        String toastMessage = Driver.getAppiumDriver().findElement(By.xpath("//android.widget.Toast[1]")).getText();
+        String toastMessage = getAppiumDriver().findElement(By.xpath("//android.widget.Toast[1]")).getText();
     }
 
+}
+  // public static void scrollToElement(String elementText, String direction)  {
+  //     AndroidDriver  driver = (AndroidDriver) getAppiumDriver();
+  //     // Find the element using the text
+  //     MobileElement element = (MobileElement) driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"" + elementText + "\").instance(0));");
+
+  //     // Get the location of the element
+  //     Point location = element.getLocation();
+
+  //     // Get the size of the element
+  //     Dimension size = element.getSize();
+
+  //     // Calculate the center point of the element
+  //     int centerX = location.x + size.width / 2;
+  //     int centerY = location.y + size.height / 2;
+
+  //     // Get the screen size
+  //     Dimension screenSize = driver.manage().window().getSize();
+  //     int screenWidth = screenSize.width;
+  //     int screenHeight = screenSize.height;
+
+  //     // Calculate the start and end points for the swipe gesture
+  //     int startX = screenWidth / 2;
+  //     int startY = screenHeight * 3 / 4;
+  //     int endX = startX;
+  //     int endY = screenHeight / 4;
+
+  //     // If direction is down, swap the start and end points
+  //     if (direction.equals("down")) {
+  //         startY = screenHeight / 4;
+  //         endY = screenHeight * 3 / 4;
+  //     }
+
+  //     // Perform the swipe gesture
+  //     driver.swipe(startX, startY, endX, endY, 1000);
+
+  //     // Check if the element is still not visible
+  //     while (!element.isDisplayed()) {
+  //         // If the element is not visible, swipe again
+  //         driver.swipe(startX, startY, endX, endY, 1000);
+  //     }
+  // }
+   //}
+
+// Use the scroll method
+//scrollToElement(driver, "Item 5","down");
 
 
-    public static void scrollDown() {
-
-        try {
-            String scrollableList="your con-desc of scrollable List";
-            String elementClassName="android.something.something";
-            String anyText="any text";
-
-            Driver.getAppiumDriver().findElement(MobileBy.AndroidUIAutomator(
-                    "new UiScrollable(new UiSelector().description(\"" + scrollableList + "\")).getChildByText("
-                            + "new UiSelector().className(\"" + elementClassName + "\"), \"" + anyText + "\")"));
-        }catch (Exception e){
-            System.out.println("Cannot scroll further");
-        }
-    }
-
-    //You can use the screen dimensions to scroll down:
-    //You can store all the images into a list using its available locator.
-    // Then use driver.scrollToExact(list.get(list.size()).getAttribute("name"));
-    //
-    //Example:
-    //
-    //List<mobileElement> images = driver.findElementsByClass("<locator>");
-    //driver.scrollToExact(images.get(images.size()).getAttribute("name"));
-    //or
-    //
-    //driver.scrollToExact(images.get(images.size()).getText());
-
-
-
-    //  @Test
-    //  public void testScroll()throws Exception
-    //  {
-    //      for(int i=0;i<4;i++)
-    //      {
-    //          Thread.sleep(2000);
-    //          if (driver.findElement(By.name("end_item")).isDisplayed())
-    //          {
-    //              driver.findElement(By.name("end_item")).click();
-    //              break;
-    //          }
-    //          else
-    //          {
-    //              horizontalScroll();
-    //          }
+//You can use the screen dimensions to scroll down:
+//You can store all the images into a list using its available locator.
+// Then use driver.scrollToExact(list.get(list.size()).getAttribute("name"));
 //
-    //      }
-    //  }
-    //  public void verticalScroll()
-    //  {
-    //      size=driver.manage().window().getSize();
-    //      int y_start=(int)(size.height*0.60);
-    //      int y_end=(int)(size.height*0.30);
-    //      int x=size.width/2;
-    //      driver.swipe(x,y_start,x,y_end,4000);
-    //  }
+//Example:
+//
+//List<mobileElement> images = driver.findElementsByClass("<locator>");
+//driver.scrollToExact(images.get(images.size()).getAttribute("name"));
+//or
+//
+//driver.scrollToExact(images.get(images.size()).getText());
+
+
+//  @Test
+//  public void testScroll()throws Exception
+//  {
+//      for(int i=0;i<4;i++)
+//      {
+//          Thread.sleep(2000);
+//          if (driver.findElement(By.name("end_item")).isDisplayed())
+//          {
+//              driver.findElement(By.name("end_item")).click();
+//              break;
+//          }
+//          else
+//          {
+//              horizontalScroll();
+//          }
+//
+//      }
+//  }
+//  public void verticalScroll()
+//  {
+//      size=driver.manage().window().getSize();
+//      int y_start=(int)(size.height*0.60);
+//      int y_end=(int)(size.height*0.30);
+//      int x=size.width/2;
+//      driver.swipe(x,y_start,x,y_end,4000);
+//  }
 
 
 
@@ -257,4 +291,3 @@ endY = screenSize["height"] * 0.25
 driver.swipe(startX, startY, endX, endY)
 
      */
-}
