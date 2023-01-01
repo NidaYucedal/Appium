981f363e0a5406acf7304693502793ad366655d5
Nida Yucedal <yucedalnida@gmail.com>
2023-01-01 12:54:55 +0300

    new

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -35,7 +35,6 @@ public class ReusableMethods {
 
 
 
-
     //elementlerin textlerine gore tiklar
     public static void tapOnElementWithText(String text) {
         List<MobileElement> mobileElementList = getAppiumDriver().findElementsByClassName("android.widget.TextView");
