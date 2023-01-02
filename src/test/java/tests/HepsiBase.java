e13e1915d91a0a84593c1c74c02c2a6e0db5984e
Nida Yucedal <yucedalnida@gmail.com>
2023-01-02 14:33:46 +0300

    new

--- src/test/java/tests/HepsiBase.java
+++ src/test/java/tests/HepsiBase.java
@@ -32,7 +32,6 @@ public class HepsiBase {
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
         return driver;
-
     }
 }
 
