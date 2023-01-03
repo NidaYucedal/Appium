a3fa648022d25a1178d4fd3cfe8b92a56189ee0a
Nida Yucedal <yucedalnida@gmail.com>
2023-01-03 12:08:44 +0300

    new

--- src/test/java/tests/HepsiBuradaApp.java
+++ src/test/java/tests/HepsiBuradaApp.java
@@ -43,7 +43,6 @@ public class HepsiBuradaApp extends HepsiBase {
 
 
 
-
         driver.findElementByXPath("(//android.widget.Button[@content-desc=\"Sepete Ekle\"])[1]").click();
         ReusableMethods.wait(5);
 
