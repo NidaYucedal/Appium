3d45fcd1dd60e987446ab417e2bfaeffab30196a
Nida Yucedal <yucedalnida@gmail.com>
2023-01-06 18:15:23 +0300

    new

--- src/test/java/tests/HepsiBuradaApp.java
+++ src/test/java/tests/HepsiBuradaApp.java
@@ -62,7 +62,6 @@ public class HepsiBuradaApp extends HepsiBase {
         driver.closeApp();
 
 
-
         /*
          // Find the element to delete
     MobileElement element = driver.findElement(By.xpath("#element-id"));
