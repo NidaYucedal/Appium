337b05791659185f48fa180edad53177d65b1913
Nida Yucedal <yucedalnida@gmail.com>
2023-01-07 16:13:27 +0300

    new

--- src/test/java/tests/HepsiBuradaApp.java
+++ src/test/java/tests/HepsiBuradaApp.java
@@ -70,7 +70,6 @@ public class HepsiBuradaApp extends HepsiBase {
 
 
 
-
         MobileElement login = driver.findElementByXPath("//btnLogin[@text='Giriş yap']");
         Assert.assertTrue(login.isDisplayed());
 
