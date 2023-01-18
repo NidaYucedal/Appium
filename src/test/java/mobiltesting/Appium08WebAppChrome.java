a1ec07c0afdc3f220e89c7a45027d5a5113836b0
Nida Yucedal <yucedalnida@gmail.com>
2023-01-18 17:33:18 +0300

    new

--- src/test/java/mobiltesting/Appium08WebAppChrome.java
+++ src/test/java/mobiltesting/Appium08WebAppChrome.java
@@ -19,7 +19,7 @@ public class Appium08WebAppChrome {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "626c3e64");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "6000");
