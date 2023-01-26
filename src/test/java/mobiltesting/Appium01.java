b16fa8b17e24e4f9e3558d52e4c1a8f6be3a71e3
Nida Yucedal <yucedalnida@gmail.com>
2023-01-26 18:59:48 +0300

    update

--- src/test/java/mobiltesting/Appium01.java
+++ src/test/java/mobiltesting/Appium01.java
@@ -13,47 +13,24 @@ import java.util.concurrent.TimeUnit;
 
 public class Appium01 {
 
-    public Appium01() throws MalformedURLException {
-
-    }
-
-
     @Test
     public void test() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        //capabilities.setCapability("platformName","Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "626c3e64");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Administrator\\Desktop\\Appium\\Gesture Tool_1.3_Apkpure.apk");
-        capabilities.setCapability("appPackage","com.google.android.apps.nexuslauncher");
-        capabilities.setCapability("appActivity","com.google.android.apps.nexuslauncher.NexusLauncherActivity");
-        //app in izinleri atlayip anasayfada acilmasini istiyorsak
-        capabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
-
-        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-       // driver.findElementByXPath("//android.view.View[@content-desc=\"Apps list\"]").click();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-        //MobileElement gesture=driver.findElementByAccessibilityId("c89a0852-657b-4a9b-983c-d2b6a7089e5a");
-       // gesture.click();
-
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-       // MobileElement isdisplayed=driver.findElementById("android:id/title");
-       // Assert.assertTrue(isdisplayed.isDisplayed());
+        //capabilities.setCapability(MobileCapabilityType.APP,"");
+        capabilities.setCapability("appPackage", "com.amazon.mShop.android.shopping");
+        capabilities.setCapability("appActivity", "com.amazon.mShop.navigation.MainActivity");
+        capabilities.setCapability("noReset", true);
+        capabilities.setCapability("disableAndroidWatchers", true);
 
 
 
-        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Chrome\"]").click();
-        MobileElement search=driver.findElementById("com.android.chrome:id/url_bar");
-        //com.android.chrome:id/search_box_text
-        search.sendKeys("hepsiBurada", Keys.ENTER);
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
 
     }
 }
